22231ab02e1cd48453695ff4bd16516c0a36feda
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2026-01-12 00:50:13 +0600

    fix wiki

--- service_wikipedia.sh
+++ service_wikipedia.sh
@@ -2,5 +2,5 @@
 source 00_vars.sh
 WORKING_DIR=$(pwd)
 
-docker create --name "$CONTAINER_NAME" --volume=${WORKING_DIR}/wikipedia/:/data -p $WIKIPEDIA_PORT:80 ghcr.io/kiwix/kiwix-serve:3.3.0 wikipedia_en_all_maxi_2022-05.zim
-docker start "$CONTAINER_NAME"
+docker create --name wikipedia --volume=${WORKING_DIR}/wikipedia/:/data -p $WIKIPEDIA_PORT:80 ghcr.io/kiwix/kiwix-serve:3.3.0 wikipedia_en_all_maxi_2022-05.zim
+docker start wikipedia
